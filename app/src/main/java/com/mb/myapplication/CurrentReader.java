4025b766ba58939036e02fe606ad9fb3fcb42e7f
Maciek <banymaciej@gmail.com>
2016-12-08 11:22:53 +0100

    second

---
+++ app/src/main/java/com/mb/myapplication/CurrentReader.java
@@ -0,0 +1,285 @@
+package com.mb.myapplication;
+
+/**
+ * Created by Maciek on 2016-11-24.
+ */
+        import java.io.BufferedReader;
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.InputStreamReader;
+        import java.util.Locale;
+
+        import android.annotation.TargetApi;
+        import android.os.Build;
+        import android.util.Log;
+
+public class CurrentReader {
+
+    static final String BUILD_MODEL = Build.MODEL.toLowerCase(Locale.ENGLISH);
+
+    @TargetApi(4)
+    static public Long getValue() {
+        File f = null;
+
+        // Galaxy S3
+        if (CurrentReader.BUILD_MODEL.contains("gt-i9300")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9300T")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9305")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9305N")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9305T")
+                || CurrentReader.BUILD_MODEL.contains("shv-e210k")
+                || CurrentReader.BUILD_MODEL.contains("shv-e210l")
+                || CurrentReader.BUILD_MODEL.contains("shv-e210s")
+                || CurrentReader.BUILD_MODEL.contains("sgh-t999")
+                || CurrentReader.BUILD_MODEL.contains("sgh-t999l")
+                || CurrentReader.BUILD_MODEL.contains("sgh-t999v")
+                || CurrentReader.BUILD_MODEL.contains("sgh-i747")
+                || CurrentReader.BUILD_MODEL.contains("sgh-i747m")
+                || CurrentReader.BUILD_MODEL.contains("sgh-n064")
+                || CurrentReader.BUILD_MODEL.contains("sc-06d")
+                || CurrentReader.BUILD_MODEL.contains("sgh-n035")
+                || CurrentReader.BUILD_MODEL.contains("sc-03e")
+                || CurrentReader.BUILD_MODEL.contains("SCH-j021")
+                || CurrentReader.BUILD_MODEL.contains("scl21")
+                || CurrentReader.BUILD_MODEL.contains("sch-r530")
+                || CurrentReader.BUILD_MODEL.contains("sch-i535")
+                || CurrentReader.BUILD_MODEL.contains("sch-S960l")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9308")
+                || CurrentReader.BUILD_MODEL.contains("sch-i939")
+                || CurrentReader.BUILD_MODEL.contains("sch-s968c")) {
+            f = new File("/sys/class/power_supply/battery/current_max");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        if (CurrentReader.BUILD_MODEL.contains("nexus 7")
+                || CurrentReader.BUILD_MODEL.contains("one")
+                || CurrentReader.BUILD_MODEL.contains("lg-d851")) {
+            f = new File("/sys/class/power_supply/battery/current_now");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        if (CurrentReader.BUILD_MODEL.contains("sl930")) {
+            f = new File("/sys/class/power_supply/da9052-bat/current_avg");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        // Galaxy S4
+        if (CurrentReader.BUILD_MODEL.contains("sgh-i337")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9505")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9500")
+                || CurrentReader.BUILD_MODEL.contains("sch-i545")
+                || CurrentReader.BUILD_MODEL.contains("find 5")
+                || CurrentReader.BUILD_MODEL.contains("sgh-m919")
+                || CurrentReader.BUILD_MODEL.contains("sgh-i537")) {
+            f = new File("/sys/class/power_supply/battery/current_now");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        if (CurrentReader.BUILD_MODEL.contains("cynus")) {
+            f = new File(
+                    "/sys/devices/platform/mt6329-battery/FG_Battery_CurrentConsumption");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+        // Zopo Zp900, etc.
+        if (CurrentReader.BUILD_MODEL.contains("zp900")
+                || CurrentReader.BUILD_MODEL.contains("jy-g3")
+                || CurrentReader.BUILD_MODEL.contains("zp800")
+                || CurrentReader.BUILD_MODEL.contains("zp800h")
+                || CurrentReader.BUILD_MODEL.contains("zp810")
+                || CurrentReader.BUILD_MODEL.contains("w100")
+                || CurrentReader.BUILD_MODEL.contains("zte v987")) {
+            f = new File(
+                    "/sys/class/power_supply/battery/BatteryAverageCurrent");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        // Samsung Galaxy Tab 2
+        if (CurrentReader.BUILD_MODEL.contains("gt-p31")
+                || CurrentReader.BUILD_MODEL.contains("gt-p51")) {
+            f = new File("/sys/class/power_supply/battery/current_avg");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        // trimuph with cm7, lg ls670, galaxy s3, galaxy note 2
+        if (CurrentReader.BUILD_MODEL.contains("triumph")
+                || CurrentReader.BUILD_MODEL.contains("ls670")
+                || CurrentReader.BUILD_MODEL.contains("gt-i9300")
+                || CurrentReader.BUILD_MODEL.contains("sm-n9005")
+                || CurrentReader.BUILD_MODEL.contains("gt-n7100")
+                || CurrentReader.BUILD_MODEL.contains("sgh-i317")) {
+            f = new File("/sys/class/power_supply/battery/current_now");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        // htc desire hd / desire z / inspire?
+        // htc evo view tablet
+        if (CurrentReader.BUILD_MODEL.contains("desire hd")
+                || CurrentReader.BUILD_MODEL.contains("desire z")
+                || CurrentReader.BUILD_MODEL.contains("inspire")
+                || CurrentReader.BUILD_MODEL.contains("pg41200")) {
+            f = new File("/sys/class/power_supply/battery/batt_current");
+            if (f.exists()) {
+                return OneLineReader.getValue(f, false);
+            }
+        }
+
+        // nexus one cyangoenmod
+        f = new File("/sys/devices/platform/ds2784-battery/getcurrent");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, true);
+        }
+
+        // sony ericsson xperia x1
+        f = new File(
+                "/sys/devices/platform/i2c-adapter/i2c-0/0-0036/power_supply/ds2746-battery/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, false);
+        }
+
+        // xdandroid
+		/* if (Build.MODEL.equalsIgnoreCase("MSM")) { */
+        f = new File(
+                "/sys/devices/platform/i2c-adapter/i2c-0/0-0036/power_supply/battery/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, false);
+        }
+		/* } */
+
+        // some htc devices
+        f = new File("/sys/class/power_supply/battery/batt_current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, true);
+        }
+
+        // Nexus One.
+        // TODO: Make this not default but specific for N1 because of the normalization.
+        f = new File("/sys/class/power_supply/battery/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, true);
+        }
+
+        // samsung galaxy vibrant
+        f = new File("/sys/class/power_supply/battery/batt_chg_current");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // sony ericsson x10
+        f = new File("/sys/class/power_supply/battery/charger_current");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // Nook Color
+        f = new File("/sys/class/power_supply/max17042-0/current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // Xperia Arc
+        f = new File("/sys/class/power_supply/bq27520/current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, true);
+
+        // Motorola Atrix
+        f = new File(
+                "/sys/devices/platform/cpcap_battery/power_supply/usb/current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // Acer Iconia Tab A500
+        f = new File("/sys/EcControl/BatCurrent");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // charge current only, Samsung Note
+        f = new File("/sys/class/power_supply/battery/batt_current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // galaxy note, galaxy s2
+        f = new File("/sys/class/power_supply/battery/batt_current_adc");
+        if (f.exists())
+            return OneLineReader.getValue(f, false);
+
+        // intel
+        f = new File("/sys/class/power_supply/max170xx_battery/current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, true);
+
+        // Sony Xperia U
+        f = new File("/sys/class/power_supply/ab8500_fg/current_now");
+        if (f.exists())
+            return OneLineReader.getValue(f, true);
+
+        f = new File("/sys/class/power_supply/android-battery/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, false);
+        }
+
+        // Nexus 10, 4.4.
+        f = new File("/sys/class/power_supply/ds2784-fuelgauge/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, true);
+        }
+
+        f = new File("/sys/class/power_supply/Battery/current_now");
+        if (f.exists()) {
+            return OneLineReader.getValue(f, false);
+        }
+
+        return null;
+    }
+
+    static private class OneLineReader {
+        static public Long getValue(File f, boolean convertToMillis) {
+
+            String text = null;
+
+            try {
+                FileInputStream fs = new FileInputStream(f);
+                InputStreamReader sr = new InputStreamReader(fs);
+                BufferedReader br = new BufferedReader(sr);
+
+                text = br.readLine();
+
+                br.close();
+                sr.close();
+                fs.close();
+            } catch (Exception ex) {
+                Log.e("CurrentWidget", ex.getMessage());
+                ex.printStackTrace();
+            }
+
+            Long value = null;
+
+            if (text != null) {
+                try	{
+                    value = Long.parseLong(text);
+                } catch (NumberFormatException nfe) 	{
+                    Log.e("CurrentWidget", nfe.getMessage());
+                    value = null;
+                }
+
+                if (convertToMillis && value != null) {
+                    value = value / 1000; // convert to milliampere
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ app/src/main/java/com/mb/myapplication/MainActivity.java
@@ -0,0 +1,359 @@
+package com.mb.myapplication;
+
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Long oldCurrent = 0L;
+    Long current = 0L;
+
+    boolean isPermissionGranted;
+
+    UsageStatsManager mUsageStatsManager;
+
+    SimpleDateFormat dataFormatter;
+
+    TextView sheduleTasks, packages, viewEvents;
+
+    ArrayList<Current> currentData = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sheduleTasks = (TextView) findViewById(R.id.textView);
+        packages = (TextView) findViewById(R.id.packages);
+        viewEvents = (TextView) findViewById(R.id.uEvents);
+//        oldCurrent = getTotalBatteryCurrent();
+
+        mUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+
+        dataFormatter = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss");
+
+        TextView permissionsView = (TextView) findViewById(R.id.permissionsView);
+        permissionsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askForPermissions();
+            }
+        });
+
+        sheduleTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sheduleBatteryCurrent();
+                sheduleUsageEvents();
+            }
+        });
+        viewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getUsageEvents(10, System.currentTimeMillis() - 1000*60*10);
+            }
+        });
+        packages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buildTv2Text(10);
+            }
+        });
+    }
+
+    private void askForPermissions() {
+        Calendar cal = Calendar.getInstance();
+        long end = cal.getTimeInMillis();
+        cal.set(Calendar.YEAR, -1);
+        TextView permissionsView = (TextView) findViewById(R.id.permissionsView);
+        long start = cal.getTimeInMillis();
+
+        if( mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, start, end).isEmpty() ) {
+            isPermissionGranted = false;
+            permissionsView.setText("PERMISSION DENIED!");
+            Log.d("LOGGER", "PYTAM!");
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        } else {
+            isPermissionGranted = true;
+            permissionsView.setText("PERMISSION GRANTED");
+            Log.d("LOGGER", "MASZ POZWOLENIA");
+        }
+    }
+
+    private void sheduleUsageEvents() {
+        Timer timer = new Timer();
+        final TextView tv = (TextView) findViewById(R.id.uEvents);
+        final long startTime = System.currentTimeMillis();
+//        final android.os.Handler handler = new android.os.Handler();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+//                Log.d("LOGGER", "Starttime: " + startTime);
+                final String text = getUsageEvents(1, startTime) ;
+                new Thread(){
+                    public void run() {
+                        tv.post(new Runnable() {
+                            public void run() {
+                                tv.setText(text);
+                            }
+                        });
+                    }
+                }.start();
+            }
+        };
+        timer.schedule(task, 0, 1000);
+    }
+
+    private String getUsageEvents(int minutes, long startTime) {
+
+//        Log.i("START", "START" + (startTime) + "current: " +  System.currentTimeMillis() + "\n");
+        long totalBackTime = 1000*60*minutes;
+
+        UsageEvents events = mUsageStatsManager.queryEvents(System.currentTimeMillis() - 1000*60*minutes , System.currentTimeMillis());
+        UsageEvents.Event event = new UsageEvents.Event();
+
+
+        SimpleDateFormat df = new SimpleDateFormat("kk:mm:ss:SSS");
+
+        StringBuilder builder = new StringBuilder();
+
+        String firstPackageName = null;
+        Long firstPackageTime = null;
+
+        String lastPackageName = null;
+        Long lastPackageTime = null;
+
+        Map<String, Long> foregroundTimeMap = new HashMap<>();
+
+        while( events.hasNextEvent()) {
+            events.getNextEvent(event);
+
+            String packageName = event.getPackageName();
+
+            final PackageManager pm = getApplicationContext().getPackageManager();
+            ApplicationInfo ai;
+            try {
+                ai = pm.getApplicationInfo( packageName, 0);
+            } catch (final PackageManager.NameNotFoundException e) {
+                ai = null;
+            }
+            final String applicationName = (String) (ai != null ? pm.getApplicationLabel(ai) : "(unknown)");
+
+            Long timeStamp = event.getTimeStamp();
+            if( timeStamp >=  System.currentTimeMillis() - 1000*60*minutes && (event.getEventType() == 1 || event.getEventType() == 2)) {
+                if( event.getEventType() == 1 ) {
+                    firstPackageName = packageName;
+                    firstPackageTime = event.getTimeStamp();
+
+                    Long timeDifference = System.currentTimeMillis() - event.getTimeStamp();
+                    if( !events.hasNextEvent() ) {
+                        Log.d("LOGGER", "nie ma eventa!");
+                        if( foregroundTimeMap.containsKey(applicationName) ) {
+                            foregroundTimeMap.put(applicationName, foregroundTimeMap.get(applicationName) + timeDifference);
+                        } else {
+                            foregroundTimeMap.put(applicationName, timeDifference);
+                        }
+                    }
+                } else if( event.getEventType() == 2 && packageName.equals(firstPackageName)) {
+                    Long timeDifference = event.getTimeStamp() - firstPackageTime;
+
+                    if( foregroundTimeMap.containsKey(applicationName) ) {
+                        foregroundTimeMap.put(applicationName, foregroundTimeMap.get(applicationName) + timeDifference);
+                    } else {
+                        foregroundTimeMap.put(applicationName, timeDifference);
+                    }
+                }
+
+                String text = "PACZKA! " + applicationName + " \t- " + df.format(new Date(timeStamp)) + ", \t event: " + event.getEventType() + "\n";
+                builder.append(text);
+//                Log.d("LOGGER", text);
+            }
+        }
+
+//        Log.d("EVENT_SIZE", "Size: " + foregroundTimeMap.size());
+
+        return mapToString(foregroundTimeMap);
+    }
+
+    private String mapToString(Map<String, Long> map) {
+        StringBuilder builder = new StringBuilder();
+        long size = 0;
+        for ( String key : map.keySet() ) {
+            builder.append( key + " : " + map.get(key) + "\n");
+            size += map.get(key);
+        }
+        builder.append("size: " + ((double) size /(1000*60)) + "\n");
+        return builder.toString();
+    }
+
+    private void buildTv2Text(int minutes) {
+
+        List<UsageStats> stats = getUsageStatistics(UsageStatsManager.INTERVAL_BEST);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("\r\n");
+        int c = 0;
+        for ( UsageStats _stats : stats ) {
+            if (_stats.getLastTimeUsed() > (System.currentTimeMillis() - (1000*60*minutes)) ) {
+                String dateString = dataFormatter.format(new Date(_stats.getLastTimeUsed()));
+                builder.append("app: ");
+                builder.append(_stats.getPackageName());
+                builder.append("\n\t\ttime: ");
+                builder.append(dateString);
+                builder.append("\t - total in fg: \t");
+                builder.append(_stats.getTotalTimeInForeground()/1000);
+                builder.append(" s\r\n");
+
+                Log.i("LOGGER", "TIME NOW: " + System.currentTimeMillis() + ", LAST STAMP: " + _stats.getLastTimeStamp() + ", LAST USED: " + _stats.getLastTimeUsed() + ", PACKAGE: " + _stats.getPackageName() );
+
+                c++;
+            }
+        }
+        ((TextView) findViewById(R.id.packages)).setText(builder.toString());
+
+        Log.d("LOGGER", "size: " + c);
+    }
+
+    private void sheduleBatteryCurrent() {
+        Timer timer = new Timer();
+        final android.os.Handler handler = new android.os.Handler();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                current = getTotalBatteryCurrent();
+                if( !current.equals(oldCurrent) ) {
+                    oldCurrent = current;
+                    Long time = System.currentTimeMillis();
+                    final double avg;
+                    currentData.add(new Current(oldCurrent, time));
+                    if( currentData.size() > 4 ) {
+                        ArrayList<Current> c = new ArrayList<>();
+                        for( int i = 5; i > 0; i-- ) {
+                            c.add(currentData.get(currentData.size() - i));
+                        }
+                        avg = calculateAverage(c);
+                    } else {
+                        avg = 0L;
+                    }
+
+//                            avg = calculateAverage(currentData.subList(Math.max(currentData.size()-5, 0), currentData.size()-1));
+                    String text = "Current(mA):\t" + oldCurrent + "\t, average(mA):\t" + avg + "\t, t(ms):\t" + (time - currentData.get(Math.max(currentData.size()-6,0)).timeInMilis) + "\n";
+                    Log.d("LOGGER", text);
+                    writeToFile(text, getApplicationContext());
+
+                    new Thread(){
+                        public void run() {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    DecimalFormat df = new DecimalFormat("#.00");
+                                    sheduleTasks.setText("Current " + df.format(oldCurrent) + " mA, avg: " + df.format(avg) + " mA, count: " + currentData.size());
+                                }
+                            });
+                        }
+                    }.start();
+
+                }
+            }
+        };
+        timer.schedule(task, 0, 1000);
+
+    }
+
+    private void writeToFile(String data,Context context) {
+        File path = getExternalFilesDir(null);
+        File file = new File(path, "log_current.txt");
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(file, true);
+//            Log.d("PATH", file.getAbsolutePath());
+            try {
+                fos.write(data.getBytes());
+            }
+            catch (IOException e) {
+                Log.e("Exception", "File write failed: " + e.toString());
+            }
+            finally {
+                fos.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Long getTotalBatteryCurrent() {
+        Long current = CurrentReader.getValue();
+//        System.out.println(current);
+        return current;
+    }
+
+    private double calculateAverage(ArrayList<Current> marks) {
+//        Log.d("LOGGER", marks.size()+"");
+        Long sum = 0L;
+        if(!marks.isEmpty()) {
+            for (Current mark : marks) {
+                sum += mark.current;
+            }
+            return sum.doubleValue() / marks.size();
+        }
+        return sum;
+    }
+
+    public List<UsageStats> getUsageStatistics(int intervalType) {
+        // Get the app statistics since one year ago from the current time.
+        List<UsageStats> queryUsageStats = mUsageStatsManager
+                .queryUsageStats(intervalType, System.currentTimeMillis() - 60 * 1000,
+                        System.currentTimeMillis());
+
+        return queryUsageStats;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LOGGER", "PAUSE");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LOGGER", "RESUME");
+        askForPermissions();
+    }
+}
+
+class Current {
+    Long current;
+    Long timeInMilis;
+
+    public Current(Long c, Long t) {
+        current = c;
+        timeInMilis = t;
+    }
+}
